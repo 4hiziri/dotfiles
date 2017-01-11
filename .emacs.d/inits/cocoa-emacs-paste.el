fc83d86435c2d901a6c91a0f43e5bc4cdf3565c4
4hiziri <meirvg@gmail.com>
2017-01-12 08:47:38 +0900

    cocoa-emacs-paste.el: enable to use kill-yank with TRAMP

--- .emacs.d/inits/cocoa-emacs-paste.el
+++ .emacs.d/inits/cocoa-emacs-paste.el
@@ -1,5 +1,11 @@
 (defun copy-from-osx ()
-  (shell-command-to-string "pbpaste"))
+  "Handle copy/paste intelligently on osx."
+  (let ((pbpaste (purecopy "/usr/bin/pbpaste")))
+    (if (and (eq system-type 'darwin)
+             (file-exists-p pbpaste))
+        (let ((tramp-mode nil)
+              (default-directory "~"))
+          (shell-command-to-string pbpaste)))))
 
 (defun paste-to-osx (text &optional push)
   (let ((process-connection-type nil))
@@ -7,5 +13,17 @@
       (process-send-string proc text)
       (process-send-eof proc))))
 
-(setq interprogram-cut-function 'paste-to-osx)
-(setq interprogram-paste-function 'copy-from-osx)
+(setq interprogram-cut-function 'paste-to-osx
+      interprogram-paste-function 'copy-from-osx)
+
+;; (defun copy-from-osx ()
+;;   (shell-command-to-string "pbpaste"))
+
+;; (defun paste-to-osx (text &optional push)
+;;   (let ((process-connection-type nil))
+;;     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+;;       (process-send-string proc text)
+;;       (process-send-eof proc))))
+
+;; (setq interprogram-cut-function 'paste-to-osx)
+;; (setq interprogram-paste-function 'copy-from-osx)
