788fd5f0c5f83ee4c19a682b3b6025b4d2aad035
4hiziri <meirvg@gmail.com>
2017-04-30 23:00:33 +0900

    disable emacs plugin for its usage

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,13 @@
-. ~/.config/fish/aliases.fish
-
 ### fzf ###
 set -U FZF_LEGACY_KEYBINDINGS 1
 ### fzf ###
+### emacs ###
+# set fish_plugins emacs
+### emacs ###
 
 . ~/.config/fish/env.fish
 
 setxkbmap -option ctrl:nocaps
 
 # xmodmap ~/.xmodmap-(uname -n)
+. ~/.config/fish/aliases.fish
