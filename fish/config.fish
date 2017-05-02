e24f4e0b04022988bb6602fe922497a0b46b8cec
tkgsy <meirvg@gmail.com>
2017-05-02 14:19:11 +0900

    add coment

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,14 @@
-. ~/.config/fish/aliases.fish
-
+### fisherman ###
+### fisherman ###
 ### fzf ###
 set -U FZF_LEGACY_KEYBINDINGS 1
 ### fzf ###
-
+### load config file ###
 . ~/.config/fish/env.fish
-
+. ~/.config/fish/aliases.fish
+### load config file ###
+### keyboard setting ###
 setxkbmap -option ctrl:nocaps
+### keyboard setting ###
 
 # xmodmap ~/.xmodmap-(uname -n)
