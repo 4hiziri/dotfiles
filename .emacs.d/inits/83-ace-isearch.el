04671e55ad52978184b3e3f569e14a0092ae27ac
tkgsy <meirvg@gmail.com>
2017-05-02 14:17:32 +0900

    enable setting

--- .emacs.d/inits/83-ace-isearch.el
+++ .emacs.d/inits/83-ace-isearch.el
@@ -4,7 +4,8 @@
 
 ;; Author: 高木聖也 <seiya@hiziri0014.local>
 ;; Keywords: local, tools, files
-
-(global-ace-isearch-mode 1)
+(use-package ace-isearch
+  :config
+  (global-ace-isearch-mode 1))
 
 ;;; 
