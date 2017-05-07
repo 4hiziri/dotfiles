98f57cef62d9cccb6a0e44773f0b6044cad1faab
4hiziri <meirvg@gmail.com>
2017-05-07 09:33:33 +0900

    refer to https://gist.github.com/julionc/7476620#file-install_phantomjs-sh

---
+++ phantomjs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ~
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+sudo tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2
+
+sudo mv phantomjs-2.1.1-linux-x86_64 /usr/local/share
+sudo ln -sf /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
