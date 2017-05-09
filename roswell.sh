e8e5b3a10bea858ad9e66c90fa01555ad6632a7f
4hiziri <meirvg@gmail.com>
2017-05-09 20:31:11 +0900

    roswell install script

---
+++ roswell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# sudo apt-get install gcc make autoconf automake libcurl3-dev
+
+cd
+git clone https://github.com/snmsts/roswell.git
+cd roswell
+./bootstrap
+./configure
+make
+make install
