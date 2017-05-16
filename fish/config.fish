af84bf2576933c3cd8846964a34cd8fbdbb0a44e
4hiziri <meirvg@gmail.com>
2017-05-16 09:18:34 +0900

    add dict setting, je and ej

--- fish/config.fish
+++ fish/config.fish
@@ -10,3 +10,13 @@ end
 ### keyboard setting ###
 setxkbmap -option ctrl:nocaps
 ### keyboard setting ###
+### dict ###
+# 辞書を引く(英和)
+function ej
+    grep "$argv" /usr/share/dict/dict -E -A 1 -wi --color=always | less -R -FX
+end
+# 辞書を引く(和英)
+function je
+    grep "$argv" /usr/share/dict/dict -E -B 1 -wi --color=always | less -R -FX
+end
+### dict ###
