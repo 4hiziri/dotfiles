d7b1391c47719eee77c1f62de7d4dbef2043a2fe
4hiziri <meirvg@gmail.com>
2017-09-26 23:11:41 +0900

    Add intsaller

---
+++ bin/install-main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+"$HOME/dotfiles/bin/install-tools.sh"
+"$HOME/dotfiles/bin/fish.sh"
+"$HOME/dotfiles/bin/fisherman.sh"
+"$HOME/dotfiles/bin/fzf.sh"
+"$HOME/dotfiles/bin/peco.sh"
+"$HOME/dotfiles/bin/install-fish-conf.sh"
+"$HOME/dotfiles/bin/phantomjs.sh"
+"$HOME/dotfiles/bin/pyenv.sh"
+"$HOME/dotfiles/bin/roswell.sh"
+"$HOME/dotfiles/bin/rustup.sh "
+
