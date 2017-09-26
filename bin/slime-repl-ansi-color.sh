0d6ec13bafef5a37ba33d6235eff083ec775aac1
4hiziri <meirvg@gmail.com>
2017-09-26 23:24:27 +0900

    modified git clone

--- bin/slime-repl-ansi-color.sh
+++ bin/slime-repl-ansi-color.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git clone git@github.com:enriquefernandez/slime-repl-ansi-color.git
+cd
+git clone https://github.com/enriquefernandez/slime-repl-ansi-color.git
 mv slime-repl-ansi-color/slime-repl-ansi-color.el $HOME/.emacs.d/.cask/2*/elpa/slime-2*/contrib/
 rm -rf slime-repl-ansi-color/
