0dc19b78531d137ac3235c3f08a3d10d72080eed
4hiziri <meirvg@gmail.com>
2017-09-29 13:08:20 +0900

    enable global-smartparens-mode

--- .emacs.d/inits/80-smartparens.el
+++ .emacs.d/inits/80-smartparens.el
@@ -1,9 +1,9 @@
 (add-hook 'emacs-lisp-mode-hook 'turn-on-smartparens-strict-mode)
 
-(use-package smartparens-config
-  :defer t
+(use-package smartparens-config 
   :bind (("C-c r" . sp-forward-slurp-sexp) ;; paredit-forward-slurp-sexp
 	 ("C-c l" . sp-forward-barf-sexp)
 	 ("C-c DEL" . backward-delete-char))
   :config
+  (smartparens-global-mode t)
   (sp-use-paredit-bindings))
