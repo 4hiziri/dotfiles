88aec32cd24274d3f93cb5ce15eaf18c81abb537
tkgsy <meirvg@gmail.com>
2017-10-11 12:52:21 +0900

    Add install script for flycheck-cargo

---
+++ bin/flycheck-cargo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd
+git clone https://github.com:hugoduncan/flycheck-cargo.git
+mkdir "$HOME/.emacs.d/elpa/"
+cp flycheck-cargo/flycheck-cargo.el "$HOME/.emacs.d/elpa/"
+rm -rf flycheck-cargo
