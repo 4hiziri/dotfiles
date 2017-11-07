18428adf8d9c1249a1190832e01b9188a106d146
4hiziri <meirvg@gmail.com>
2017-11-07 13:06:49 +0900

    Add global

---
+++ bin/install-global.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd
+yse | sudo apt-get install exuberant-ctags
+wget http://tamacom.com/global/global-6.5.7.tar.gz
+tar xvf global-6.5.7.tar.gz
+cd global-6.5.7
+./configure --with-exuberant-ctags=/usr/bin/ctags-exuberant
+make
+sudo make install
+
+cd ..
+mv global-6.5.7 src/global
