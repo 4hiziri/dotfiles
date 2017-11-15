aa11dee7a911ea2b763cc23d00830171df287584
tkgsy <meirvg@gmail.com>
2017-11-15 10:13:47 +0900

    Add rustup commands

--- bin/rustup.sh
+++ bin/rustup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 curl https://sh.rustup.rs -sSf | sh
-. $HOME/.cargo/env 
-rustup component add rust-src
+. $HOME/.cargo/env
+
+# rustup install nightly
+# rustup component add rust-src
+# rustup completions fish > ~/.config/fish/completions/rustup.fish
