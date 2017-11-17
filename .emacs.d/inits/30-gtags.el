cae6e3456c0522fd9d5f1b1ae1a2730b4fa6e546
tkgsy <meirvg@gmail.com>
2017-11-17 10:48:11 +0900

    Modified gtags function for c-mode-map

--- .emacs.d/inits/30-gtags.el
+++ .emacs.d/inits/30-gtags.el
@@ -4,7 +4,7 @@
   (setq helm-gtags-path-style 'root)
   (setq helm-gtags-auto-update t)
   :config
-  (bind-key "M-." 'helm-gtags-dwim c-mode-map)
+  (bind-key "M-." 'helm-gtags-dwim c-mode-map) ;; TODO: fix
   (bind-key "M-s" 'helm-gtags-show-stack c-mode-map)
   (bind-key "M-p" 'helm-gtags-previous-history c-mode-map)
   (bind-key "M-n" 'helm-gtags-next-history c-mode-map))
