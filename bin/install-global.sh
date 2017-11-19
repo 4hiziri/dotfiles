bb35b3de8bf2b75a94b93ef5722b773da0802274
4hiziri <meirvg@gmail.com>
2017-11-19 10:21:04 +0900

    Fix to remove tarball

--- bin/install-global.sh
+++ bin/install-global.sh
@@ -4,6 +4,7 @@ cd
 yes | sudo apt-get install exuberant-ctags
 wget http://tamacom.com/global/global-6.5.7.tar.gz
 tar xvf global-6.5.7.tar.gz
+rm global-6.5.7.tar.gz
 cd global-6.5.7
 ./configure --with-exuberant-ctags=/usr/bin/ctags-exuberant
 make
