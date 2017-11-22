375db0b520b21a41f1aae5007e960c8f33a14af6
tkgsy <meirvg@gmail.com>
2017-11-22 14:18:27 +0900

    Add install software

--- bin/fish.sh
+++ bin/fish.sh
@@ -1,7 +1,10 @@
-# TODO: check fish version
+#!/bin/bash
 
 sudo apt-get update
 sudo apt-get install apt-file
+sudo apt-file update
+sudo apt-file add-apt-repository
+sudo apt-get install software-properties-common
 
 sudo apt-add-repository ppa:fish-shell/release-2
 sudo apt-get update
