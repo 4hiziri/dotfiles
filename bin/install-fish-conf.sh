bec7650e1a165dd725fb1e36708a73daf1ade65e
4hiziri <meirvg@gmail.com>
2017-11-26 15:20:31 +0900

    Modified link

--- bin/install-fish-conf.sh
+++ bin/install-fish-conf.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 if [ -d "$HOME/.config/fish" ]
-then
-    cd "$HOME/dotfiles/fish/"
-    for f in `ls | grep ".*\.fish"`
-    do
-	ln -sf "$HOME/dotfiles/fish/$f" "$HOME/.config/fish/$f"
-    done
-
+then    
+    ln -sf "$HOME/dotfiles/fish/aliases.fish" "$HOME/.config/fish/aliases.fish"
+    ln -sf "$HOME/dotfiles/fish/config.fish" "$HOME/.config/fish/config.fish"
+    ln -sf "$HOME/dotfiles/fish/env.fish" "$HOME/.config/fish/env.fish"
     ln -sf "$HOME/dotfiles/fish/fishfile" "$HOME/.config/fish/fishfile"
-    ln -sf "$HOME/dotfiles/fish/functions/fish_user_key_bindings.fish" "$HOME/.config/fish/functions/fish_user_key_bindings.fish"    
+    ln -sf "$HOME/dotfiles/fish/functions/fish_user_key_bindings.fish" "$HOME/.config/fish/functions/fish_user_key_bindings.fish"
 else
     echo "fish shell does not exists."
 fi
