8cf5d074e8b2c8a8ce2f930703c9c9fdf30eb015
4hiziri <meirvg@gmail.com>
2017-12-18 14:13:03 +0900

    Make setup script

--- bin/setup-env.sh
+++ bin/setup-env.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-"$HOME/dotfiles/bin/install-tools.sh"
-"$HOME/dotfiles/bin/fish.sh"
-"$HOME/dotfiles/bin/fisherman.sh"
-"$HOME/dotfiles/bin/fzf.sh"
-"$HOME/dotfiles/bin/peco.sh"
-"$HOME/dotfiles/bin/install-fish-conf.sh"
-"$HOME/dotfiles/bin/phantomjs.sh"
-"$HOME/dotfiles/bin/pyenv.sh"
-"$HOME/dotfiles/bin/roswell.sh"
-"$HOME/dotfiles/bin/rustup.sh"
-"$HOME/dotfiles/bin/install-emacs.sh"
-mv -f "$HOME/.emacs.d/" "$HOME/.emacs.d.bak"
-"$HOME/dotfiles/bin/install-emacs-d.sh"
-"$HOME/dotfiles/bin/cask.sh"
-"$HOME/dotfiles/bin/slime-repl-ansi-color.sh"
+cd "$HOME/dotfiles"
+./setup-fish.sh
+./setup-emacs.sh
