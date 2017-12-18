5c73e18c1bb4e1db53a1c042a52bbb8a69c28a26
4hiziri <meirvg@gmail.com>
2017-12-18 12:58:56 +0900

    Delete unnecessary line.

--- bin/fish.sh
+++ bin/fish.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install apt-file
-sudo apt-file update
-sudo apt-file add-apt-repository
-sudo apt-get install software-properties-common
-
-sudo apt-add-repository ppa:fish-shell/release-2
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository -y -u ppa:fish-shell/release-2
 sudo apt-get update
-sudo apt-get install fish
+sudo apt-get -y install fish
