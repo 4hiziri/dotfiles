0e45a198514a89e79aa679982ae835ca5ca00c9b
4hiziri <meirvg@gmail.com>
2018-02-06 20:33:13 +0900

    Add comment

--- ctf/retdec.sh
+++ ctf/retdec.sh
@@ -3,12 +3,13 @@
 sudo apt-get install build-essential cmake git perl python3 bash bison flex autoconf automake libtool pkg-config m4 coreutils zlib1g-dev libtinfo-dev wget bc upx doxygen graphviz
 
 cd
-mkdir src > /dev/null
+mkdir src 2> /dev/null
 cd src
 
 git clone https://github.com/avast-tl/retdec
 cd retdec
 mkdir build && cd build
+# need cmake >= 3.6
 cmake .. # -DCMAKE_INSTALL_PREFIX=<path>
 make -j4
-make install
+sudo make install
