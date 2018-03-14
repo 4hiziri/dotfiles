77075a8a0ad498e95899e0ac2b31e19e8f4c4265
tkgsy <meirvg@gmail.com>
2018-03-14 17:34:40 +0900

    Without X

--- bin/emacs.sh
+++ bin/emacs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get -y install gcc make ncurses-dev libxml2-dev libgif-dev libxpm-dev gnutls-bin
+sudo apt-get -y install gcc make ncurses-dev libxml2-dev libgif-dev libxpm-dev gnutls-bin libjpeg-dev libtiff-dev
 
 cd
 ver="25.3"
@@ -10,6 +10,6 @@ rm -f "emacs-$ver.tar.gz"
 mkdir 'src' > /dev/null
 mv "emacs-$ver" 'src/emacs'
 cd 'src/emacs'
-./configure
+./configure --with-x-toolkit=no
 sudo make
 sudo make install
