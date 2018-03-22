0ea9cde2523441643ff9ccebb4962d354d50b900
4hiziri <meirvg@gmail.com>
2018-03-22 22:03:07 +0900

    Add zsh completion

--- bin/setup-rust.sh
+++ bin/setup-rust.sh
@@ -8,4 +8,7 @@ rustup install nightly
 rustup component add rust-src
 rustup completions fish > ~/.config/fish/completions/rustup.fish
 
+sudo mkdir /usr/local/share/zsh-completions/
+rustup completions zsh | sudo tee -a /usr/local/share/zsh-completions/_rustup 
+
 ./rust-env.sh
