9e5613a9d80ecd1b15c1db243cf9a72a2f8cf45b
tkgsy <none>
2018-05-05 07:32:24 +0000

    Update download source

--- bin/rbenv.sh
+++ bin/rbenv.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-git clone git@github.com:sstephenson/rbenv.git ~/.rbenv
-mkdir $HOME/.rbenv/plugins
-git clone git@github.com:sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+# install rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+cd ~/.rbenv && src/configure && make -C src
+
+# verify rbenv install
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
+
+# install ruby-build
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"'
 echo 'eval "$(rbenv init -)"'
