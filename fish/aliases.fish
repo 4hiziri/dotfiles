eff036a5c60525effeb5c6dda71f194d1e472a39
tkgsy <meirvg@gmail.com>
2018-05-30 08:23:55 +0900

    Update alias

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,15 +1,15 @@
 balias emacs '/usr/local/bin/emacsclient -nw -a ""'
 balias ekill '/usr/local/bin/emacsclient -e (kill-emacs)'
 
-alias rm 'rm -i'
 alias cp 'cp -i'
 alias mv 'mv -i'
 
 alias ls 'ls -F --color=auto'
 
-balias mkdirp 'mkdir -p'
+# balias mkdirp 'mkdir -p'
+alias mkdir 'mkdir -p'
 
-balias sudo 'sudo '
+# balias sudo 'sudo '
 
 # for ctf
 balias exploit 'python exploit.py'
