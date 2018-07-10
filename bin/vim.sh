a3490fe75ae3dcff0b58f1eb8817ff7b07982f24
4hiziri <meirvg@gmail.com>
2018-07-10 23:37:13 +0900

    Add vim

---
+++ bin/vim.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt install -y git build-essential libtinfo-dev python3-dev libxmu-dev libxpm-dev libgtk-3-dev
+
+cd
+cd src
+git clone https://github.com/vim/vim.git
+cd vim
+./configure --enable-python3interp=dynamic --enable-fail-if-missing
+make
+sudo make install
+sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 100
+sudo update-alternatives --install /usr/bin/vim vim /usr/local/bin/vim 100
