3e5856d8a62caeb48dc36d7a2f421e69e7163f1f
tkgsy <meirvg@gmail.com>
2018-07-11 19:53:28 +0900

    Add plugin manager install

--- bin/vim.sh
+++ bin/vim.sh
@@ -11,3 +11,5 @@ make
 sudo make install
 sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 100
 sudo update-alternatives --install /usr/bin/vim vim /usr/local/bin/vim 100
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
