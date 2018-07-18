4e82c70ff847619ca26bb14bf3a659c09eacf856
tkgsy <meirvg@gmail.com>
2018-07-18 09:17:50 +0900

    Add -y

--- bin/docker.sh
+++ bin/docker.sh
@@ -2,14 +2,14 @@
 # for debian
 
 # Dependency
-sudo apt install  apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 #Add key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Add repository
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # Install
 sudo apt update
-sudo apt install docker-ce
+sudo apt install -y docker-ce
