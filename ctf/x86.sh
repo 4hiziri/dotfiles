4c2bd295247f98e72a0c9bb2d8214e68ae900933
4hiziri <meirvg@gmail.com>
2018-08-16 09:41:51 +0900

    Update

--- ctf/x86.sh
+++ ctf/x86.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install ia32-libs lib32gcc1 lib32stdc++6 libc6-dev-i386 gcc-multilib
+sudo dpkg --add-architecture i386
+sudo apt install libc6:i386 libncurses5:i386 libstdc++6:i386 gcc-multilib multiarch-support
