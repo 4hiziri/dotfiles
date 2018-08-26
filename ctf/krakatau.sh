0f32fb41b98a0716240015f3f8825b6cef264f80
4hiziri <meirvg@gmail.com>
2018-08-26 13:38:39 +0900

    Fix command link

--- ctf/krakatau.sh
+++ ctf/krakatau.sh
@@ -4,4 +4,6 @@
 
 cd "$HOME/src/"
 git clone 'https://github.com/Storyyeller/Krakatau'
-ln -sf "$HOME/src/Krakatau" "$HOME/.local/bin/"
+ln -sf "$HOME/src/Krakatau/assemble.py" "$HOME/.local/bin/"
+ln -sf "$HOME/src/Krakatau/disassemble.py" "$HOME/.local/bin/"
+ln -sf "$HOME/src/Krakatau/compile.py" "$HOME/.local/bin/"
