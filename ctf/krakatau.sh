b74842ba88588396561fb6fd7ac8b4ee9a56de92
4hiziri <meirvg@gmail.com>
2018-08-26 15:48:40 +0900

    Fix path problem

--- ctf/krakatau.sh
+++ ctf/krakatau.sh
@@ -4,6 +4,12 @@
 
 cd "$HOME/src/"
 git clone 'https://github.com/Storyyeller/Krakatau'
+
+# for pyenv problem, fix shebang
+sed -i '1c#!/usr/bin/python2' 'Krakatau/assemble.py'
+sed -i '1c#!/usr/bin/python2' 'Krakatau/disassemble.py'
+sed -i '1c#!/usr/bin/python2' 'Krakatau/decompile.py'
+
 ln -sf "$HOME/src/Krakatau/assemble.py" "$HOME/.local/bin/"
 ln -sf "$HOME/src/Krakatau/disassemble.py" "$HOME/.local/bin/"
-ln -sf "$HOME/src/Krakatau/compile.py" "$HOME/.local/bin/"
+ln -sf "$HOME/src/Krakatau/decompile.py" "$HOME/.local/bin/"
