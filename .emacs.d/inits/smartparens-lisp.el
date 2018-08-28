fee778d923a04ae27ff395f44e7241e85a50c1c3
4hiziri <meirvg@gmail.com>
2018-08-28 23:07:13 +0900

    Fix for straight

--- .emacs.d/inits/smartparens-lisp.el
+++ .emacs.d/inits/smartparens-lisp.el
@@ -1,5 +1,3 @@
-(use-package smartparens)
-
 (sp-with-modes '(lisp-mode lisp-interaction-mode slime-mode slime-repl-mode emacs-lisp-mode)
   (sp-local-pair "'" nil :actions nil)
   (sp-local-pair "`" nil :actions nil))
