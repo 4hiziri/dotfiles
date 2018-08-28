4c4062ffea796e23e9ad59d4dc8a3aeec034de61
4hiziri <meirvg@gmail.com>
2018-08-28 22:17:24 +0900

    Update emacs

--- bin/emacs.sh
+++ bin/emacs.sh
@@ -3,7 +3,7 @@
 sudo apt-get -y install gcc make ncurses-dev libxml2-dev libgif-dev libxpm-dev gnutls-bin libjpeg-dev libtiff-dev libpng-dev libacl1-dev
 
 cd
-ver="25.3"
+ver="26.1"
 wget "http://ftp.jaist.ac.jp/pub/GNU/emacs/emacs-$ver.tar.gz"
 tar xvf "emacs-$ver.tar.gz"
 rm -f "emacs-$ver.tar.gz"
