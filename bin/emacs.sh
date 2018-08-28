a7204bce4ac4ec2208e0401fd5db7743199620a5
4hiziri <meirvg@gmail.com>
2018-08-28 23:08:54 +0900

    Fix script

--- bin/emacs.sh
+++ bin/emacs.sh
@@ -3,13 +3,13 @@
 sudo apt-get -y install gcc make ncurses-dev libxml2-dev libgif-dev libxpm-dev gnutls-bin libjpeg-dev libtiff-dev libpng-dev libacl1-dev
 
 cd
+mkdir 'src' 2>&1 > /dev/null
+cd src
 ver="26.1"
 wget "http://ftp.jaist.ac.jp/pub/GNU/emacs/emacs-$ver.tar.gz"
 tar xvf "emacs-$ver.tar.gz"
 rm -f "emacs-$ver.tar.gz"
-mkdir 'src' > /dev/null
-mv "emacs-$ver" 'src/emacs'
-cd 'src/emacs'
+cd "emacs-$ver"
 ./configure --with-x-toolkit=no
 make
 sudo make install
