aecdefe4778b985678eb47bc87c131daeeeb5292
tkgsy <meirvg@gmail.com>
2018-10-26 15:35:50 +0900

    Update

--- .emacs.d/inits/30-gtags.el
+++ .emacs.d/inits/30-gtags.el
@@ -1,12 +1,13 @@
-(use-package cc-mode)
+(use-package cc-mode
+  :defer t)
 
 (use-package helm-gtags
   :init
-  (add-hook 'c-mode-hook 'helm-gtags-mode)
+  (add-hook 'c-mode-common-hook 'helm-gtags-mode)
   (setq helm-gtags-path-style 'root)
-  (setq helm-gtags-auto-update t)    
+  (setq helm-gtags-auto-update t)
   :config
-  (bind-key "M-." 'helm-gtags-dwim c-mode-map) ;; TODO: fix
+  (bind-key "M-." 'helm-gtags-dwim c-mode-map)
   (bind-key "M-s" 'helm-gtags-show-stack c-mode-map)
   (bind-key "M-p" 'helm-gtags-previous-history c-mode-map)
   (bind-key "M-n" 'helm-gtags-next-history c-mode-map))
