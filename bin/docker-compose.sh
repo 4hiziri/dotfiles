ed09a8bb026a050db5cc6591fe018f2c2ce8e9fd
4hiziri <meirvg@gmail.com>
2018-11-13 23:26:06 +0900

    Change hardcoded version number to variable

--- bin/docker-compose.sh
+++ bin/docker-compose.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+VER='1.23.1'
 
-sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${VAR}/docker-compose-$(uname -s)-$(uname -m)" -o '/usr/local/bin/docker-compose'
+
+sudo chmod +x '/usr/local/bin/docker-compose'
 
 # completion
 ## for bash
-sudo curl -L https://raw.githubusercontent.com/docker/compose/1.22.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+sudo curl -L "https://raw.githubusercontent.com/docker/compose/${VER}/contrib/completion/bash/docker-compose" -o '/etc/bash_completion.d/docker-compose'
 ## for zsh
-mkdir -p ~/.zsh/completion
-curl -L https://raw.githubusercontent.com/docker/compose/1.22.0/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
+mkdir -p "$HOME/.zsh/completion"
+curl -L "https://raw.githubusercontent.com/docker/compose/${VER}/contrib/completion/zsh/_docker-compose" > "$HOME/.zsh/completion/_docker-compose"
