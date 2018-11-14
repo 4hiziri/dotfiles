5ca792ab9932e07e9e2ba5ed2e39d0e9cb914129
4hiziri <meirvg@gmail.com>
2018-11-14 23:08:26 +0900

    Fix typo

--- bin/docker-compose.sh
+++ bin/docker-compose.sh
@@ -2,7 +2,7 @@
 
 VER='1.23.1'
 
-sudo curl -L "https://github.com/docker/compose/releases/download/${VAR}/docker-compose-$(uname -s)-$(uname -m)" -o '/usr/local/bin/docker-compose'
+sudo curl -L "https://github.com/docker/compose/releases/download/${VER}/docker-compose-$(uname -s)-$(uname -m)" -o '/usr/local/bin/docker-compose'
 
 sudo chmod +x '/usr/local/bin/docker-compose'
 
