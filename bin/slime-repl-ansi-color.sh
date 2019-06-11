f10a1def286577561fe7633ea7cd6c6e48b2563b
tkgsy <meirvg@gmail.com>
2019-06-11 18:26:52 +0900

    Update for straight

--- bin/slime-repl-ansi-color.sh
+++ bin/slime-repl-ansi-color.sh
@@ -2,5 +2,5 @@
 
 cd
 git clone https://github.com/enriquefernandez/slime-repl-ansi-color.git
-mv slime-repl-ansi-color/slime-repl-ansi-color.el $HOME/.emacs.d/.cask/2*/elpa/slime-2*/contrib/
+mv slime-repl-ansi-color/slime-repl-ansi-color.el $HOME/.emacs.d/straight/repos/slime/contrib/
 rm -rf slime-repl-ansi-color/
