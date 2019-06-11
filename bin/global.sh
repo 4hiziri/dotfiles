604250d25fac0021b1cfcde4d2f1afada7a69b40
tkgsy <meirvg@gmail.com>
2019-06-11 18:26:16 +0900

    Remove version info

--- bin/global.sh
+++ bin/global.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+VER='6.6.3'
+
 cd
-yes | sudo apt-get install exuberant-ctags
-wget http://tamacom.com/global/global-6.5.7.tar.gz
-tar xvf global-6.5.7.tar.gz
-rm global-6.5.7.tar.gz
-cd global-6.5.7
+sudo apt install -y exuberant-ctags
+wget "https://ftp.gnu.org/pub/gnu/global/global-${VER}.tar.gz"
+tar xvf "global-${VER}.tar.gz"
+rm "global-${VER}.tar.gz"
+cd "global-${VER}"
 ./configure --with-exuberant-ctags=/usr/bin/ctags-exuberant
 make
 sudo make install
 
 cd ..
-mv global-6.5.7 src/global
+mv "global-${VER}" src/global
