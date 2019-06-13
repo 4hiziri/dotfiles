491718132a40fe504fb576de97fd4a43d6d579fd
tkgsy <meirvg@gmail.com>
2019-06-13 13:47:55 +0900

    Make version dependent

--- bin/go.sh
+++ bin/go.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-wget 'https://dl.google.com/go/go1.10.linux-amd64.tar.gz'
-tar xvf 'go1.10.linux-amd64.tar.gz'
-sudo mv go /usr/local/
-rm 'go1.10.linux-amd64.tar.gz'
+VER="1.12.6"
+
+cd "$HOME/src/"
+
+wget "https://dl.google.com/go/go${VER}.linux-amd64.tar.gz"
+tar xvf "go${VER}.linux-amd64.tar.gz"
+sudo cp -r go '/usr/local/'
+rm "go${VER}.linux-amd64.tar.gz"
