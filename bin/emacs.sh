6d50e35976f67d4437226bdf9605ffb7b06b700a
4hiziri <meirvg@gmail.com>
2019-12-15 20:43:58 +0900

    Update package

--- bin/emacs.sh
+++ bin/emacs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo apt-get -y install gcc make ncurses-dev libxml2-dev libgif-dev libxpm-dev libgnutls-dev gnutls-bin gnutls-dev libjpeg-dev libtiff-dev libpng-dev libacl1-dev build-essential libcurses-dev mailutils
+sudo apt-get -y install gcc make ncurses-dev libxml2-dev libgif-dev libxpm-dev gnutls-bin gnutls-dev libjpeg-dev libtiff-dev libpng-dev libacl1-dev build-essential mailutils
 
 cd
 mkdir 'src' 2>&1 > /dev/null
 cd src
-ver="26.1"
+ver="26.3"
 wget "http://ftp.jaist.ac.jp/pub/GNU/emacs/emacs-$ver.tar.gz"
 tar xvf "emacs-$ver.tar.gz"
 rm -f "emacs-$ver.tar.gz"
