ccbba76dc74c80bb00a8a39bca6c14318b4e4f71
4hiziri <meirvg@gmail.com>
2021-04-05 16:28:52 +0900

    update new method

--- bin/cask.sh
+++ bin/cask.sh
@@ -3,13 +3,12 @@
 if [ -d "$HOME/.emacs.d/" ]
 then
     sudo apt-get -y install curl python gnutls-bin
-    curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
-    # TODO: bash config
-    export PATH="$HOME/.cask/bin:$PATH"
+	
+	git clone https://github.com/cask/cask ~/.cask
+    PATH=$HOME/.cask/bin:$PATH
 
-    cd
     cask upgrade-cask
-    cd ~/.emacs.d
+    cd "$HOME/.emacs.d"
     cask install  
 else
     echo 'plz install emacs!'
