466e3c46d0b491fbb729394cb878100fc51daa58
4hiziri <meirvg@gmail.com>
2022-09-03 00:09:37 +0900

    Decide to use straight instead of cast and pallet

--- .emacs.d/inits/01-cask.el
+++ .emacs.d/inits/01-cask.el
@@ -1,14 +1,14 @@
 ;;; cask-config --- Summary
 ;;; Commentary:
-
 ;;; Code:
 
-(use-package cask
-  :load-path
-  "~/.cask/cask.el"
-  :config
-  (cask-initialize))
+;; (use-package cask
+;;   :load-path
+;;   "~/.cask/cask.el")
 
-(use-package pallet)
+;; (use-package pallet)
+
+;; cask is obsolete already
+;; use straight
 
 ;;; 01-cask.el ends here
