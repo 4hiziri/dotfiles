94af447c00358f672eb20036e8758ee92db17a8d
4hiziri <meirvg@gmail.com>
2022-09-02 23:01:08 +0900

    Minor change

--- .emacs.d/inits/00-init.el
+++ .emacs.d/inits/00-init.el
@@ -4,6 +4,7 @@
 ;;; Code:
 
 ;; ミニバッファに時計を表示
+(eval-when-compile (defvar display-time-string-forms)) ;; to suppress error
 (setq display-time-string-forms
 	  '((format "%04d/%02d/%02d(%s) %02d:%02d"
 				(string-to-number year)
@@ -24,11 +25,12 @@
 (setq auto-save-file-name-transforms
 	  `((".*" ,(expand-file-name "~/.emacs.d/backups/") t)))
 
+;; 大昔のサンプルコード、不要だが参考のため残す
 ;;dtwをdelete-trailing-whitespaceのエイリアスにする
 (defalias 'dtw 'delete-trailing-whitespace)
 
 ;;履歴拡張
-(setq history-length 1000)
+(setq history-length 3000)
 
 ;;キーストローク
 (setq echo-keystrokes 0.1)
@@ -36,7 +38,12 @@
 ;;ffap
 (ffap-bindings)
 
-;;uniquify
+;;template
+(auto-insert-mode)
+(defvar auto-insert-directory "~/projects/dotfiles/.emacs.d/insert/")
+(define-auto-insert "\\.c" "c-template.c")
+
+;; misc packages
 (use-package uniquify
   :config
   (defvar uniquify-buffer-name-style 'post-forward-andle-brackets)
@@ -45,16 +52,10 @@
   (defvar uniquify-buffer-name-style 'post-forward-angle-brackets)
   :straight nil)
 
-;;template
-(auto-insert-mode)
-(defvar auto-insert-directory "~/projects/dotfiles/.emacs.d/insert/")
-(define-auto-insert "\\.c" "c-template.c")
-
-;; linum
 (use-package linum
   :config
   (global-linum-mode)
-  (setq linum-format "%d "))
+  (setq linum-format "%2d |"))
 
 (use-package w3m
   :defer t)
@@ -62,8 +63,8 @@
 (use-package sequential-command
   :defer t)
 
-;; tab-width
 (setq-default tab-width 4)
+
 (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120))
 
 ;;; 00-init.el ends here
