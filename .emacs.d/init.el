125b4dbb57433fdfae7d23510a52815bc00dc0ff
4hiziri <meirvg@gmail.com>
2022-09-02 16:41:05 +0900

    Fix format

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,11 +15,11 @@
 
 ;; load-path を追加する関数を定義
 (defun add-to-load-path (paths)
-  "LOAD-PATHを追加する関数."
+  "LOAD-PATHにPATHSを追加する."
   (dolist (default-directory paths paths)
     (add-to-list 'load-path default-directory)
     (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-	(normal-top-level-add-subdirs-to-load-path))))
+		(normal-top-level-add-subdirs-to-load-path))))
 
 ;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
 ;; 存在するディレクトリに対してのみpathに追加
