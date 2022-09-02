d94ba4dc08d65ab37e4fd9bec6ea07add97b4edb
4hiziri <meirvg@gmail.com>
2022-09-03 00:10:17 +0900

    add description

--- .emacs.d/inits/30-gtags.el
+++ .emacs.d/inits/30-gtags.el
@@ -1,4 +1,10 @@
+;;; gtags --- helm gtags
+;;; Commentary:
+;;; Code:
+
 (use-package helm-gtags
   :init
   (setq helm-gtags-path-style 'root)
   (setq helm-gtags-auto-update t))
+
+;;; 30-gtags.el ends here
