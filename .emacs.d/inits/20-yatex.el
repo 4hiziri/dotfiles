7257742bf144afa75cbe03b3582dbf8d10a4db11
hiziri <meirvg+dev@gmail.com>
2022-09-08 15:32:24 +0900

    Disabled

--- .emacs.d/inits/20-yatex.el
+++ .emacs.d/inits/20-yatex.el
@@ -1,9 +1,11 @@
-;;; YaTeX
+;;; YaTeX --- TeX mode
+;;; Commentary:
 
 ;;; Code:
 
 ;; yatex-mode の起動
 (use-package yatex
+  :disabled t
   :mode ("\\.tex$" . yatex-mode)
   :init
   (setq YaTeX-kanji-code nil))
@@ -14,3 +16,5 @@
 ;; 2: ISO-2022-JP (other default)
 ;; 3: EUC
 ;; 4: UTF-8
+
+;;; 20-yatex.el ends here
