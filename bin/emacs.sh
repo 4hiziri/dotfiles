7ff81b4e1a3c61ed2f2835d2ae738b0c3898e574
takagi <takagi@cyberdefense.jp>
2022-09-08 14:13:29 +0900

    Update for emacs-28.1

--- bin/emacs.sh
+++ bin/emacs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ver="28.1"
 
-sudo apt-get -y install gcc make ncurses-dev libxml2-dev gnutls-bin gnutls-dev libpng-dev libacl1-dev build-essential mailutils
+sudo apt-get -y install gcc make ncurses-dev libxml2-dev gnutls-bin gnutls-dev libpng-dev libacl1-dev build-essential mailutils libgccjit-10-dev
 
 cd
 mkdir 'src' 2>&1 > /dev/null
@@ -10,6 +10,7 @@ wget "http://ftp.jaist.ac.jp/pub/GNU/emacs/emacs-$ver.tar.gz"
 tar xvf "emacs-$ver.tar.gz"
 rm -f "emacs-$ver.tar.gz"
 cd "emacs-$ver"
-./configure --with-cairo --without-x --without-dbus --without-gconf --without-gsettings --without-toolkit-scroll-bars --without-xaw3d --without-sound --without-pop --without-xpm --without-tiff --without-rsvg --without-selinux --without-gpm --with-native-compilation --with-gnutls --without-gif
+./autogen.sh
+CFLAGS='-I/usr/lib/gcc/x86_64-linux-gnu/10/include -L/usr/lib/gcc/x86_64-linux-gnu/10' ./configure --with-cairo --without-x --without-dbus --without-gconf --without-gsettings --without-toolkit-scroll-bars --without-xaw3d --without-sound --without-pop --without-xpm --without-tiff --without-rsvg --without-selinux --without-gpm --with-native-compilation --with-gnutls --without-gif
 make
 sudo make install
