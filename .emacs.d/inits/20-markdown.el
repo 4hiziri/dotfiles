f4b37deb37b45081b5166a6150eaf2a81bdb1b3b
takagi <takagi@cyberdefense.jp>
2022-09-09 23:03:50 +0900

    Merged

--- .emacs.d/inits/20-markdown.el
+++ .emacs.d/inits/20-markdown.el
@@ -6,25 +6,14 @@
 (use-package w3m
   :defer t)
 
-(use-package w3m)
-(use-package markdown-mode)
-
-(define-key markdown-mode-map "\C-c\C-cm"
-    (lambda ()
-      (interactive)
-      (w3m-find-file (buffer-file-name))))
-
-(setq markdown-fontify-code-blocks-natively t)
-
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-=======
 (use-package markdown-mode
   :defer t
   :mode ("\\.md\\'" . markdown-mode)
   :bind (:map markdown-mode-map
 			  ("C-c C-c m" . (lambda ()
 							   (interactive)
-							   (w3m-find-file (buffer-file-name))))))
->>>>>>> 522423c1abf1ae16ec440f1d424db5b7f70e9b5e
+							   (w3m-find-file (buffer-file-name)))))
+  :config
+  (setq markdown-fontify-code-blocks-natively t))
 
 ;;; 20-markdown.el ends here
