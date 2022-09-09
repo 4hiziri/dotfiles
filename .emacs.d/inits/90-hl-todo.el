30e36feb81e7e7ca317d06426537933737298f2a
4hiziri <meirvg@gmail.com>
2022-09-09 23:07:37 +0900

    Merge

--- .emacs.d/inits/90-hl-todo.el
+++ .emacs.d/inits/90-hl-todo.el
@@ -1,17 +1,12 @@
 ;;; hl-todo --- highlight todo
 ;;; Commentary:
+
 ;;; Code:
 
 (use-package hl-todo
   :config
   (push 'emacs-lisp-mode hl-todo-activate-in-modes)
-  (global-hl-todo-mode 1))
-
-;;; 90-hl-todo.el ends here
-=======
-(use-package hl-todo  
-  :config
-  (push 'emacs-lisp-mode hl-todo-activate-in-modes)
   (global-hl-todo-mode 1)  
   (add-to-list 'hl-todo-keyword-faces '("WARN" . "#f1e928")))
->>>>>>> bdaeed2f160be86d1e2c9072efca4435fb74117b
+
+;;; 90-hl-todo.el ends here
