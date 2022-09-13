e729972531254a0d8ba012e8f9f75a5474119ddb
hiziri <test@example.com>
2022-09-13 17:29:13 +0900

    Fix

--- bin/cica.sh
+++ bin/cica.sh
@@ -1,6 +1,10 @@
+[Isudo apt install unar
 cd "$HOME"
 wget 'https://github.com/miiton/Cica/releases/download/v5.0.3/Cica_v5.0.3.zip'
 unar Cica_v5.0.3.zip
 cd Cica_v5.0.3
-cp *.ttf ~/.local/share/fonts/
+mkdir -p "$HOME/.local/share/fonts/Cica"
+cp *.ttf "$HOME/.local/share/fonts/Cica"
 fc-cache -f -v
+cd ../
+rm -rf 'Cica_v5.0.3/' 'Cica_v5.0.3.zip'
