05a2320be5b294e852bcedb4bc2422ab250e2692
takagi <takagi@cyberdefense.jp>
2022-09-14 10:14:00 +0900

    Remove obsolate commands

--- bin/setup-go.sh
+++ bin/setup-go.sh
@@ -4,12 +4,12 @@ cd $HOME/dotfiles/bin/
 ./go.sh
 ./goenv.sh 
 
-if which go > /dev/null
-then
-    go get github.com/rogpeppe/godef
-    go get -u github.com/nsf/gocode
-    go get github.com/golang/lint/golint
-    go get github.com/kisielk/errcheck
-else
-    echo "go command doesn't exists"
-fi
+# if which go > /dev/null
+# then
+#     go get github.com/rogpeppe/godef
+#     go get -u github.com/nsf/gocode
+#     go get github.com/golang/lint/golint
+#     go get github.com/kisielk/errcheck
+# else
+#     echo "go command doesn't exists"
+# fi
