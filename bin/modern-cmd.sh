b353bbec462668022af4af8933e331940de52374
takagi <takagi@cyberdefense.jp>
2022-09-14 13:58:18 +0900

    Add command

--- bin/modern-cmd.sh
+++ bin/modern-cmd.sh
@@ -1,3 +1,8 @@
 sudo apt install bat
 cargo install exa
 cargo install du-dust
+cd $HOME/src/
+git clone 'https://github.com/muesli/duf.git'
+cd duf
+go build
+sudo ln -s $HOME/src/duf/duf /usr/local/bin/duf
