6b6856e405ec35fa1925d2cfcb70a2732c6c65f3
takagi <takagi@cyberdefense.jp>
2022-09-14 14:13:07 +0900

    Update

--- bin/uim.sh
+++ bin/uim.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo apt install uim-xim uim-skk uim-pref-gtk3
+sudo apt install uim-xim uim-skk skktools
+cd $HOME/src/
+git clone 'https://github.com/skk-dev/dict'
+cd dict
+skkdic-expr2 SKK-JISYO.L + SKK-JISYO.propernoun + SKK-JISYO.jinmei + SKK-JISYO.geo > SKK-JISYO.MERGED
+/bin/bash $HOME/dotfiles/script/k2c.sh SKK-JISYO.MERGED > SKK-JISYO.k2c
+skkdic-expr2 SKK-JISYO.L + SKK-JISYO.propernoun + SKK-JISYO.jinmei + SKK-JISYO.geo + SKK-JISYO.k2c > SKK-JISYO.MERGED
